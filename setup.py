d3bce08cf8ed204a55f050c48a16ff270cb5b5f9
twneale <twneale@gmail.com>
2016-06-10 01:04:00 -0400

    Ignore stuff, delete cruft

--- setup.py
+++ setup.py
@@ -25,21 +25,7 @@ test_requirements = [
 ]
 
 
-class InstallPy4jdbc(install):
-    """Customized setuptools install command - prints a friendly greeting."""
-    def run(self):
-        self.sbt_assembly()
-        install.run(self)
-
-    def sbt_assembly(self):
-        cwd = os.getcwd()
-        os.chdir('scala')
-        subprocess.check_call('sbt assembly', shell=True)
-        os.chdir(cwd)
-
-
 setup(
-    cmdclass={'install': InstallPy4jdbc},
     name='py4jdbc',
     version='0.1.0',
     description="py4j JDBC wrapper",
