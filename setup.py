25454c4a16747effdb332107d94f993e4f4a5a13
Randall Schwager <rschwager@massmutual.com>
2017-02-28 14:52:19 -0500

    fix: automatically write MANIFEST.in; also make README a real file

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ import subprocess
 from setuptools import setup
 from distutils.command.build import build
 from distutils.command.clean import clean
+from distutils.command.sdist import sdist
 from distutils.spawn import find_executable
 
-with open('README.rst') as readme_file:
+with open('README') as readme_file:
     readme = readme_file.read()
 
 requirements = ["py4j==0.10.1"]
@@ -18,6 +19,8 @@ test_requirements = ["pytest==2.9.2", "coverage==4.1", "pytest-cov==2.3.0"]
 
 exec(compile(open("py4jdbc/version.py").read(), "py4jdbc/version.py", 'exec'))
 VERSION = __version__  # noqa
+ASSEMBLY_JAR = "py4jdbc/scala/target/scala-2.10/py4jdbc-assembly-{0}.jar".format(__version__)
+
 
 class jar_build(build):
     def run(self):
@@ -52,6 +55,16 @@ class jar_clean(clean):
         subprocess.check_call('sbt clean', shell=True)
         os.chdir(cwd)
 
+
+class my_sdist(sdist):
+    def initialize_options(self, *args, **kwargs):
+        here = os.path.dirname(os.path.abspath(__file__))
+        filename = os.path.join(here, "MANIFEST.in")
+        with open(filename, 'w') as f:
+            incld = "include {}\n"
+            f.write(incld.format(ASSEMBLY_JAR))
+        return super().initialize_options(*args, **kwargs)
+
 setup(
     name='py4jdbc',
     version=VERSION,
@@ -78,7 +91,8 @@ setup(
     tests_require=test_requirements,
     cmdclass={
         'build': jar_build,
-        'clean': jar_clean
+        'clean': jar_clean,
+        'sdist': my_sdist,
     },
     package_data={
         'py4jdbc': [
@@ -89,7 +103,7 @@ setup(
         ]
     },
     data_files=[
-        ('share/py4jdbc', ["py4jdbc/scala/target/scala-2.10/py4jdbc-assembly-{0}.jar".format(__version__)])
+        ('share/py4jdbc', [ASSEMBLY_JAR])
     ],
     setup_requires=setup_requirements
 )
